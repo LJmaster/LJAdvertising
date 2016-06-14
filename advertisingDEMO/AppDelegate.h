b96ca47cd156b20f6ea2ebf9fa4da0a861833074
liujie <liujie@liujiedemacbook-pro.local>
2016-06-14 10:16:45 +0800

    Initial Commit

---
+++ advertisingDEMO/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  advertisingDEMO
+//
+//  Created by liujie on 16/6/14.
+//  Copyright © 2016年 liujie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ advertisingDEMO/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  advertisingDEMO
+//
+//  Created by liujie on 16/6/14.
+//  Copyright © 2016年 liujie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
